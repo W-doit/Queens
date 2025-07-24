50c5a703be9184b1e9eff7e76e1b698f15abe3ec
Pili <pilarriusm@gmail.com>
2025-07-24 14:06:30 +0200

    fixing products not in use (as we are using iframe) for deployment

--- app/productos/[id]/page.tsx
+++ app/productos/[id]/page.tsx
@@ -1,18 +1,18 @@
-import { fetchProductoById, mockProductos } from "@/lib/odoo";
-import ProductDetailClient from "./ProductDetailClient";
+// import { fetchProductoById, mockProductos } from "@/lib/odoo";
+// import ProductDetailClient from "./ProductDetailClient";
 
-export async function generateStaticParams() {
-  return mockProductos.map(product => ({
-    id: product.id.toString(),
-  }));
-}
+// export async function generateStaticParams() {
+//   return mockProductos.map(product => ({
+//     id: product.id.toString(),
+//   }));
+// }
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const product = await fetchProductoById(params.id);
-
-  if (!product) {
-    return <div className="p-8 text-center">Producto no encontrado</div>;
-  }
-
-  return <ProductDetailClient product={product} />;
-}
+// export default async function Page({ params }: { params: { id: string } }) {
+//   const product = await fetchProductoById(params.id);
+//
+//   if (!product) {
+//     return <div className="p-8 text-center">Producto no encontrado</div>;
+//   }
+//
+//   return <ProductDetailClient product={product} />;
+// }
